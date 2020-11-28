fdc381aa08f99ef461cec51cbe6836a3a97851e2
wlam0195 <70173685+wlam0195@users.noreply.github.com>
2020-11-28 03:33:40 -0500

    Add files via upload

---
+++ TheirStories/europe-nav.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+	$(".east").mouseenter(function(){
+		$(".displayeastinfo").addClass("show");
+	});
+
+	$(".east").mouseleave(function(){
+		$(".displayeastinfo").removeClass("show");
+	});
+
+	$(".south").mouseenter(function(){
+		$(".displaysouthinfo").addClass("show");
+	});
+
+	$(".south").mouseleave(function(){
+		$(".displaysouthinfo").removeClass("show");
+	});
+
+
+	$(".west").mouseenter(function(){
+		$(".displaywestinfo").addClass("show");
+	});
+
+	$(".west").mouseleave(function(){
+		$(".displaywestinfo").removeClass("show");
+	});
+
+	$(".north").mouseenter(function(){
+		$(".displaynorthinfo").addClass("show");
+	});
+
+	$(".north").mouseleave(function(){
+		$(".displaynorthinfo").removeClass("show");
+	});
+
+});
