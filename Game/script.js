2e6a5b7a01442cee60156b8c7c1b29b05993756d
wlam0195 <70173685+wlam0195@users.noreply.github.com>
2021-03-11 14:56:48 -0500

    Add files via upload

---
+++ Game/script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var mugen = new MUGEN();
+  mugen.initialize({titleScreenImageURL: "./img/title-screen.png", actionRadius: 100, timeBetweenNPCs: 3200, npcCollisionRadius: 100, actionDuration:600, jumpHeight: 300});
+});
