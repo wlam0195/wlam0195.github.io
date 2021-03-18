d47c18596b1b6d55c90428f6514654aa26cac002
wlam0195 <70173685+wlam0195@users.noreply.github.com>
2021-03-18 15:24:55 -0400

    Delete Game directory

--- Game/script.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var mugen = new MUGEN();
-  mugen.initialize({titleScreenImageURL: "./img/title-screen.png", actionRadius: 100, timeBetweenNPCs: 3200, npcCollisionRadius: 100, actionDuration:800, npcSpeed: 300, jumpHeight: 250});
-});
